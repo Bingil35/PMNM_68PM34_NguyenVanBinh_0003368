1de633781e4982c7efbfa0d49192a110304d497b
N-Bình <nguyenvanbinh352005@gmail.com>
2026-06-10 01:34:37 +0700

    feat<sinhvien>: create Sinhvien

--- app/models/sinhvienModel.php
+++ app/models/sinhvienModel.php
@@ -1,17 +1,32 @@
 <?php
-require_once '../app/core/DB.php';
-class sinhvienModel {
-    private $conn;
+    require_once '../app/core/DB.php';
+    class SinhvienModel{
+        private $conn;
 
-    public function __construct($conn) {
-        $this->conn = ConnectDB::Connect();
-    }
-    public function getAllSinhVien(){
-        $query = "SELECT * FROM tbl_sinhviens";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+        public function __construct(){
+            $this->conn = ConnectDB::Connect();
+        }
 
-}
+        public function getAllSinhvien(){
+            $query = "SELECT * FROM tbl_sinhviens";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function create($hoten, $gioitinh, $mssv) {
+            $query = "INSERT INTO tbl_sinhviens (hoten, gioitinh, mssv) VALUES (:hoten, :gioitinh, :mssv)";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(':hoten', $hoten);
+            $stmt->bindParam(':gioitinh', $gioitinh);
+            $stmt->bindParam(':mssv', $mssv);
+            
+            if($stmt->execute()) { 
+                return true;
+                    
+            } else {
+                return false;
+            }
+        }
+    }
 ?>
